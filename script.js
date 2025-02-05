061ced20e0234b43b64070c4c717198038544433
OwenLyden <149121585+OwenLyden@users.noreply.github.com>
2025-02-06 08:11:46 +1000

    prod

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ document.addEventListener("DOMContentLoaded", () => {
             words = data.words;
             targetWord = words[Math.floor(Math.random() * words.length)];
             console.log(targetWord);
-            alert("Target word: " + targetWord);
+            alert("Start Guessing");
         })
         .catch(error => console.error('Error loading JSON:', error));
 
